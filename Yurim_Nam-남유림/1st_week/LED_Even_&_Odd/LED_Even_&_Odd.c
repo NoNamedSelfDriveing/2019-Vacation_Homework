7ae959cf778e942c7e7afc106597d81dbf3f84b4
milkyway002 <37265093+milkyway002@users.noreply.github.com>
2019-01-25 16:17:17 +0900

    과제 4번 까지 완료

---
+++ Yurim_Nam-남유림/1st_week/LED_Even_&_Odd/LED_Even_&_Odd.c
@@ -0,0 +1,16 @@
+﻿#include <asf.h>
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main (void) {
+	
+	while (1) {
+		DDRA = 0xff;
+		
+		PORTA = 0b10101010;
+		_delay_ms(1000);
+		PORTA = 0b01010101;
+		_delay_ms(1000);
+	}
+}
---
+++ Yurim_Nam-남유림/1st_week/R_F_LED1/R_F_LED1.c
@@ -0,0 +1,26 @@
+﻿#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main (void) {
+	
+	int i, o, t, h;
+	while (1) {
+		DDRA = 0xff;
+		h = 0b00000000;
+		for (i=0; i<=4; i++) {
+			PORTA = h;
+			o = 0b00000001 << i;
+			t = 0b10000000 >> i;
+			h = h | (o | t);
+			_delay_ms(1000);
+		}
+		for (i=0; i<=4; i++) {
+			PORTA = h;
+			o = 0b00001000 >> i;
+			t = 0b00010000 << i;
+			h = h ^ (o | t);
+			_delay_ms(1000);
+		}
+	}
+}
